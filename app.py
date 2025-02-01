160e028627216d51fd720ae624dabf7ff1ea3141
sharkie1704 <ishrakmellah@gmail.com>
2025-02-01 14:30:07 -0500

    create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Hello World! This is our ConUHacks IV project.")
+st.write("This is a simple Streamlit app that displays a map of Montreal.")
+st.text_area("Please enter your name:")
+st.balloons()
+st.button("Click me!")
